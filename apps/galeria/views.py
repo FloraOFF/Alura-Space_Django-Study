771625e52aeb5ce5e2506a6ad8caf798273f84ca
FloraOFF <117243848+FloraOFF@users.noreply.github.com>
2025-04-13 11:39:07 -0500

    Deleção de fotografias realizadas com sucesso

--- apps/galeria/views.py
+++ apps/galeria/views.py
@@ -68,5 +68,5 @@ def editar_imagem(request, foto_id):
 def deletar_imagem(request, foto_id):
     fotografia = Fotografia.objects.get(id = foto_id)
     fotografia.delete()
-    messages.sucess(request, 'Deleção feita com sucesso!')
+    messages.success(request, 'Deleção feita com sucesso!')
     return redirect('index')
